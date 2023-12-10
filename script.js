fb5795f2c6f052da8b27a29c7041f42e0b89db86
Adarsh <adarshofficial.1607@gmail.com>
2023-12-10 21:23:47 +0530

    adding buttons

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 // Deciding the last date of the countdown...
-const userInput = document.querySelector('.user__input-date')
+const userInput = document.querySelector(".user__input-date");
+let end;
 
 // Count down code
 const inputTime = document.querySelectorAll(".countdown__details input");
 function clock() {
-  const end = new Date(userInput.value);
   const now = new Date();
   const diff = (end - now) / 1000;
 
-  if (diff < 0) return;
+  if (diff <= 0 || isNaN(diff)) return;
 
-  inputTime[0].value = Math.floor(diff / 3600 / 24);
   inputTime[1].value = Math.floor(diff / 3600) % 24;
+  inputTime[0].value = Math.floor(diff / 3600 / 24);
   inputTime[2].value = Math.floor(diff / 60) % 60;
   inputTime[3].value = Math.floor(diff) % 60;
 }
 
-const button = document.querySelector("button");
-button.addEventListener("click", function () {
-  setInterval(() => clock(), 1000);
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
+
+let start;
+function startInterval(){
+  start = setInterval(() => clock(), 1000);
+}
+function stopInterval(){
+  clearInterval(start)
+}
+
+startBtn.addEventListener("click", function () {
+  end = new Date(userInput.value);
+  startInterval()
+});
+stopBtn.addEventListener("click", function () {
+  stopInterval()
+});
+
+resetBtn.addEventListener("click", function () {
+  stopInterval()
+  if (userInput.value !== 0) {
+    userInput.value = "";
+    inputTime[1].value = 0;
+    inputTime[0].value = 0;
+    inputTime[2].value = 0;
+    inputTime[3].value = 0;
+  }
 });
